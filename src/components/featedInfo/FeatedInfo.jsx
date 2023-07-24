62d2bfdc70b7655560467a92c70a9f4c1c09cc03
AchaUmbaPretre <achandambi@gmail.com>
2023-07-24 15:48:53 +0100

    2ieme commit

--- src/components/featedInfo/FeatedInfo.jsx
+++ src/components/featedInfo/FeatedInfo.jsx
@@ -21,6 +21,36 @@ const FeatedInfo = () => {
                         <span>*************************</span>
                     </div>
                 </div>
+
+                <div className="feated-row">
+                    <div className="feated-top">
+                        <div className="feated-row-title">
+                            <h3 className="feated-h2">Personnel</h3>
+                            <span className="feated-span">4</span>
+                        </div>
+                        <div className="feated-right">
+                            <PersonOutlineIcon/>
+                        </div>
+                    </div>
+                    <div className="feated-bottom">
+                        <span>*************************</span>
+                    </div>
+                </div>
+
+                <div className="feated-row">
+                    <div className="feated-top">
+                        <div className="feated-row-title">
+                            <h3 className="feated-h2">Personnel</h3>
+                            <span className="feated-span">4</span>
+                        </div>
+                        <div className="feated-right">
+                            <PersonOutlineIcon/>
+                        </div>
+                    </div>
+                    <div className="feated-bottom">
+                        <span>*************************</span>
+                    </div>
+                </div>
             </div>
         </div>
     </>
