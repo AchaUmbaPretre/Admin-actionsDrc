17258dc8f670ccd3bf2715ebe361eadbcb8ac6f0
AchaUmbaPretre <achandambi@gmail.com>
2023-07-24 14:59:04 +0100

    premier

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,26 @@
 import './sidebar.scss'
+import {Link} from 'react-router-dom'
+import HomeIcon from '@mui/icons-material/Home';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+import ChecklistRtlIcon from '@mui/icons-material/ChecklistRtl';
+import FactCheckIcon from '@mui/icons-material/FactCheck';
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
 
 const Sidebar = () => {
   return (
     <>
         <div className="sidebar">
-            
+          <div className="sidebar-wrapper">
+            <ul className="sidebar-ul">
+              <li className="sidenav-li"><HomeIcon className='sidebar-icon'/><Link>Accueil</Link></li>
+              <li className="sidenav-li"><PersonOutlineIcon className='sidebar-icon'/><Link>Personnel</Link></li>
+              <li className="sidenav-li"><ShowChartIcon className='sidebar-icon'/><Link>Affectation</Link></li>
+              <li className="sidenav-li"><ChecklistRtlIcon className='sidebar-icon'/><Link>Presence</Link></li>
+              <li className="sidenav-li"><FactCheckIcon className='sidebar-icon'/><Link>Facturation</Link></li>
+              <li className="sidenav-li"><FormatListNumberedIcon className='sidebar-icon'/><Link>Liste de congé</Link></li>
+            </ul>
+          </div>
         </div>
 
     </>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import './topbar.scss'
+import actions from './../../assets/actionssarl.PNG'
+import admin from '../../assets/maxim_nguz.PNG'
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <>
+      <div className="topbar">
+        <div className="topbar-left">
+            <img src={actions} alt="" className='topbar-img-title' />
+        </div>
+        <div className="topbar-right">
+          <MailOutlineIcon className='topbar-icon'/>
+          <NotificationsNoneIcon className='topbar-icon'/>
+          <img src={admin} alt="" className="topbar-img" />
+          <div className="topbar-row">
+            <PowerSettingsNewIcon className='topbar-power'/>
+            <span className="span-logout">Logout</span>
+          </div>
+        </div>
+      </div>
+
+    </>
   )
 }
 
