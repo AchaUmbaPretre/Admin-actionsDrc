a695719a82a50fe509caa829f6b2e21306787fb3
AchaUmbaPretre <achandambi@gmail.com>
2023-07-24 13:16:10 +0100

    premier commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider, Route, Outlet, Navigate } from 'react-router-dom'
+import Sidebar from './components/sidebar/Sidebar';
+import Topbar from './components/topbar/Topbar';
+import Login from './pages/login/Login';
+import './app.css'
+import Rightbar from './pages/rightbar/Rightbar';
+import User from './pages/user/User';
 
 function App() {
+
+  const Layout = () =>{
+    return(
+      <div>
+        <Topbar/>
+        <div className="appContainer">
+          <Sidebar/>
+          <div className="appOutlet">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout/>,
+      children: [
+    ]
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+ 
+      <div>
+        <RouterProvider router={router} />
+      </div>
+
   );
 }
 
