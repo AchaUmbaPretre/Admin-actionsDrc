a12c13a9403c58fcfdd5fb181d34643d5dbb8732
AchaUmbaPretre <achandambi@gmail.com>
2023-07-24 16:46:24 +0100

    commit

--- src/components/featedList/FeatedList.jsx
+++ src/components/featedList/FeatedList.jsx
@@ -1,6 +1,9 @@
 import './featedList.scss'
+import {data} from '../../data'
+import { useState } from 'react'
 
 const FeatedList = () => {
+    const [list, setList] = useState(data)
   return (
     <>
         <div className="featedList">
@@ -16,9 +19,18 @@ const FeatedList = () => {
                     <th>Etat civil</th>
                     <th>Actions</th>
                 </tr>
+                { list.map((item) =>
                 <tr>
-                    <td></td>
-                </tr>
+                    <td>{item.nom}</td>
+                    <td>{item.postnom}</td>
+                    <td>{item.prenom}</td>
+                    <td>{item.sexe}</td>
+                    <td>{item.adresse}</td>
+                    <td>{item.fonction}</td>
+                    <td>{item.etat}</td>
+                    <td>{item.action}</td> 
+                </tr> 
+                )}
             </table>
         </div>
     </>
---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const data = [
+    {
+        nom: "Ndambi",
+        postnom: "umba",
+        prenom: "Acha",
+        sexe: "m",
+        adresse: "debonhomme",
+        fonction: 'developpeur',
+        etat : "celibataire",
+        action: 'actions'
+    },
+    {
+        nom: "Ndambi",
+        postnom: "umba",
+        prenom: "Acha",
+        sexe: "m",
+        adresse: "debonhomme",
+        fonction: 'developpeur',
+        etat : "celibataire",
+        action: 'actions'
+    },
+    {
+        nom: "Ndambi",
+        postnom: "umba",
+        prenom: "Acha",
+        sexe: "m",
+        adresse: "debonhomme",
+        fonction: 'developpeur',
+        etat : "celibataire",
+        action: 'actions'
+    },
+    {
+        nom: "Ndambi",
+        postnom: "umba",
+        prenom: "Acha",
+        sexe: "m",
+        adresse: "debonhomme",
+        fonction: 'developpeur',
+        etat : "celibataire",
+        action: 'actions'
+    },
+    {
+        nom: "Ndambi",
+        postnom: "umba",
+        prenom: "Acha",
+        sexe: "m",
+        adresse: "debonhomme",
+        fonction: 'developpeur',
+        etat : "celibataire",
+        action: 'actions'
+    },
+
+]
