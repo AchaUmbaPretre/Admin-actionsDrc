0b76e63d6dedb074716b16d05c8def27fe333c51
AchaUmbaPretre <achandambi@gmail.com>
2023-07-24 16:02:38 +0100

    3ieme commit

--- src/components/featedInfo/FeatedInfo.jsx
+++ src/components/featedInfo/FeatedInfo.jsx
@@ -11,44 +11,44 @@ const FeatedInfo = () => {
                     <div className="feated-top">
                         <div className="feated-row-title">
                             <h3 className="feated-h2">Personnel</h3>
-                            <span className="feated-span">4</span>
+                            <span className="feated-span">50</span>
                         </div>
                         <div className="feated-right">
-                            <PersonOutlineIcon/>
+                            <PersonOutlineIcon className='feated-icon'/>
                         </div>
                     </div>
                     <div className="feated-bottom">
-                        <span>*************************</span>
+                        <span>*******  **********  ********</span>
                     </div>
                 </div>
 
                 <div className="feated-row">
                     <div className="feated-top">
                         <div className="feated-row-title">
-                            <h3 className="feated-h2">Personnel</h3>
-                            <span className="feated-span">4</span>
+                            <h3 className="feated-h2">Affectation</h3>
+                            <span className="feated-span">10</span>
                         </div>
                         <div className="feated-right">
-                            <PersonOutlineIcon/>
+                            <PersonOutlineIcon className='feated-icon'/>
                         </div>
                     </div>
                     <div className="feated-bottom">
-                        <span>*************************</span>
+                        <span>*******  *********  *********</span>
                     </div>
                 </div>
 
                 <div className="feated-row">
                     <div className="feated-top">
                         <div className="feated-row-title">
-                            <h3 className="feated-h2">Personnel</h3>
-                            <span className="feated-span">4</span>
+                            <h3 className="feated-h2">Liste de congé</h3>
+                            <span className="feated-span">05</span>
                         </div>
                         <div className="feated-right">
-                            <PersonOutlineIcon/>
+                            <PersonOutlineIcon className='feated-icon'/>
                         </div>
                     </div>
                     <div className="feated-bottom">
-                        <span>*************************</span>
+                        <span>*******  **********  ********</span>
                     </div>
                 </div>
             </div>
