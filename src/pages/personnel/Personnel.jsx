54402cacb60754696344e1276775da097e281e7a
AchaUmbaPretre <achandambi@gmail.com>
2023-07-25 12:53:02 +0100

    commit

--- src/pages/personnel/Personnel.jsx
+++ src/pages/personnel/Personnel.jsx
@@ -8,7 +8,26 @@ import PeopleIcon from '@mui/icons-material/People';
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import { useState } from 'react';
 import { rows } from '../../data';
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    bgcolor: 'background.paper',
+    border: '1px solid #555',
+    boxShadow: 24,
+    p: 4,
+  }
 
 const Personnel = () => {
     const [data, setData] = useState(rows);
@@ -18,6 +37,10 @@ const Personnel = () => {
       setData(dataFilter)
     }
 
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     const columns = [
         { field: 'id', headerName: 'ID', width: 70 },
         { field: 'nom', headerName: 'Nom', width: 110 },
@@ -26,7 +49,7 @@ const Personnel = () => {
           field: 'age',
           headerName: 'Age',
           type: 'number',
-          width: 90,
+          width: 100,
         },
         {
             field: 'genre',
@@ -60,15 +83,44 @@ const Personnel = () => {
         <div className="personnel">
             <div className="personnel-rows">
                 <div className="personnel-top">
-                    <PeopleIcon/>
+                    <PeopleIcon className='personnel-icon'/>
                     <div className="personnel-info">
                         <h2 className="personnel-title">Employé</h2>
                         <span className="personnel-span">Liste des employés</span>
                     </div>
                 </div>
-                <div className="personnel-bottom">
-                    <button className="personnel-btn"><PersonAddAlt1Icon/></button>
-                </div>
+                <button className="personnel-btn" onClick={handleOpen}><PersonAddAlt1Icon/>Ajouter</button>
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    slots={{ backdrop: Backdrop }}
+                    slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                    }} 
+                >
+                    <Fade in={open}>
+                        <Box sx={style}>
+                            <Box component="form" sx={{'& > :not(style)': { m: 1, width: '30ch' },}} noValidate autoComplete="off">
+                                <TextField id="filled-basic" label="Nom" variant="filled" />
+                                <TextField id="filled-basic" label="Prenom" variant="filled" />
+                                <TextField id="filled-basic" label="Date" variant="filled" />
+                                <TextField id="filled-basic" label="Email" variant="filled" />
+                                <TextField id="filled-basic" label="Id unique" variant="filled" />
+                                <TextField id="filled-basic" label="Type de pièce" variant="filled" />
+                                <TextField id="filled-basic" label="Numero de pièce " variant="filled" />
+                                <TextField id="filled-basic" label="Competence" variant="filled" />
+                                <TextField id="filled-basic" label="Certificat" variant="filled" />
+                                <TextField id="filled-basic" label="Status" variant="filled" />
+                                <TextField id="filled-basic" label="Status" variant="filled" />
+                            </Box>
+                        </Box>
+                    </Fade>
+                </Modal>
             </div>
             <DataGrid rows={data} columns={columns} pageSize={10} checkboxSelection className="userTable" />
         </div>
